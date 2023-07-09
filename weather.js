b275af3f253b318c27ea413e041ee1ab322300e3
edm1001 <edmer_franciz@yahoo.com>
2023-07-09 11:04:41 -0500

    add data param

--- weather.js
+++ weather.js
@@ -1,7 +1,48 @@
-//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FChicago
+import axios from "axios";
+
+//https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime
 
 
 
 export function getWeather (lat, lon, timezone) {
+  return  axios.get(
+    "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime", 
+    {
+        params: {
+            latitude: lat,
+            longtitude: lon,
+            timezone,
+        }
+    }).then(({data}) => {
+        return {
+            current: parseCurrentWeather(data),
+            daily: paseDailyWeather(data),
+            hourly: paseHourWeather(data)
+        }
+    })
+}
+function parseCurrentWeather({current_weather, daily }) {
+    const {
+        temperature: currentTemp, 
+        windspeed: windSpeed,
+        weatherCode: iconCode
+    } = current_weather
+    const {
+        temperature_2m_max: [maxTemp],
+        temperature_2m_min: [minTemp],
+        apparent_temperature_max: [maxFeelsLike],
+        apparent_temperature_min: [minFeelsLike],
+        precipitation_sum: [precip]
+    } = daily
 
+    return {
+        currentTemp,
+        highTemp: maxTemp ,
+        lowTemp: minTemp ,
+        highFeelsLike: maxFeelsLike ,
+        lowFeelsLike: minFeelsLike,
+        windSpeed,
+        precip,
+        iconCode,
+    }
 }

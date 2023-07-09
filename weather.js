70a42a07357952983d44b595a4b80ea16450004a
edm1001 <edmer_franciz@yahoo.com>
2023-07-09 10:19:51 -0500

    add icons

---
+++ weather.js
@@ -0,0 +1,7 @@
+//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FChicago
+
+
+
+export function getWeather (lat, lon, timezone) {
+
+}
